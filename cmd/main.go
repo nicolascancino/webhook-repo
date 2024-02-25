4d1cef952d1535cc3684799188522ee66ed83cd8
Nicolas Cancino <ncancino7@gmail.com>
2024-02-25 00:13:38 -0300

    probando algo del channel

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,30 @@
 package main
 
 import (
+	"fmt"
 	"github.com/joho/godotenv"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
 	_ = godotenv.Load()
 	mux := http.NewServeMux()
 
-	if err := http.ListenAndServe(":"+os.Getenv("PORT"), mux); err != nil {
-		log.Print(err)
-	}
+	errs := make(chan error)
+	go func() {
+		stopChan := make(chan os.Signal, 1)
+		signal.Notify(stopChan, syscall.SIGINT, syscall.SIGTERM)
+		errs <- fmt.Errorf("%s", <-stopChan)
+	}()
+
+	go func() {
+		if err := http.ListenAndServe(":"+os.Getenv("PORT"), mux); err != nil {
+			log.Fatalf("HTTP server ListenAndServe: %v", err)
+		}
+	}()
+	<-errs
 }
